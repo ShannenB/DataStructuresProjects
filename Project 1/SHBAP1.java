2fa474488f9298d9cd5d52ae5b85e818d66ff328
Shannen Barrameda <sbarrameda17@gmail.com>
2018-09-07 10:47:33 -0500

    insert, delete, and search working

--- Project 1/SHBAP1.java
+++ Project 1/SHBAP1.java
@@ -1,6 +1,7 @@
 //Shannen Barrameda sib170130
 //CS3345.HON
 //Data Structures and Algorithms Project 1
+
 import java.util.*;
 import java.io.*;
 import java.lang.Math.*;
@@ -98,7 +99,7 @@ class HashTable {
 		entries = 0;
 		L = new LinkedList[size];
 		for(int i=0;i<size;i++)
-		L[i] = new LinkedList();
+			L[i] = new LinkedList();
 	}	
 
 	int remainderUnsigned(int dividend, int divisor){
@@ -109,41 +110,65 @@ class HashTable {
 		int dividend = 0;
 		
 		for(int i = 0; i < ky.length(); i++){
-			dividend += (int)ky.charAt(i) * (int)Math.pow(31, ky.length() - i);
+			dividend += (int)ky.charAt(i) * (int)Math.pow(31, ky.length() - i -1);
 		}
 		
 		return remainderUnsigned((int)dividend, (int)Math.pow(2,32)) % tableSize;
 	}
 	
-/* 	boolean insert(String key, long val) {
+	boolean insert(String ky, long val) {
+		int index = hash(ky, this.tableSize);
+		
+		if(L[index].insert(ky, val)){
+			entries++;	//increments when each record is added
+			return true;
+        }
+		else
+			return false;
 	// attempt to insert a record. Return false if
 	// the key is already present in the table
 	// . . .
 	}
-	boolean delete(String key) {
+	boolean delete(String ky) {
+		int index = hash(ky, tableSize);
+		
+		for(int i = 0; i < tableSize; i++){
+			if(L[index].find(ky) != 0) //searches for record with the key we're looking for
+				L[index].delete(ky);	//deletes that record
+		}
+		return  false;
 	// attempt to delete a record. Return false if
 	// the key isn’t present in the table
 	// . . .
-	}
-	long search(String key) {
+	} 
+	long search(String ky) {
+		int index = hash(ky,tableSize);
+		
+		for(int i = 0; i < tableSize; i++){
+			if(L[index].find(ky) != 0)
+				return L[index].find(ky);
+		}
+        return -1;
 	// attempt to find a record. Return the value
 	// or -1 if the key is not found
 	// . . .
-	}
+	} 
 	void clearTable() {
+		entries = 0;
 	// empty the hash table
 	// . . .
 	}
 	int size() {
+		return entries;
 	// return the number of records in the table
 	// . . .
 	}
 	void printStats() {
-	// . . .
-	} */
+		System.out.println("Number of records in table = " + size());
+	}  
 } // end of class HashTable
 
-class SHBAP1 { // use a class name according to your name. See Rules for Programming . . .
+class SHBAP1 { 
 	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(new File("P1data.txt"));
 		String s = sc.nextLine();
@@ -151,37 +176,39 @@ class SHBAP1 { // use a class name according to your name. See Rules for Program
 		HashTable HT = new HashTable(M);
 		s = sc.nextLine();
 		int q = Integer.parseInt(s); //number of command lines
+		
 		for(int i=0;i<q;i++) {
 		s = sc.nextLine();
 		String [] tokens = s.split(" ");
 		String command = tokens[0];
-		
-		LinkedList ll = new LinkedList();
-		ll.insert("see ya", 40);
-		ll.insert("hi", 12347);
-		ll.insert("bye", 12345);
-		ll.insert("bye", 4);
-		System.out.println(ll.delete("hi"));
-		System.out.println(HT.hash("ABCD", 16));
 		
 
 			 switch(command) { 
 				case "I": // insert
 					String key = tokens[1];
 					long val = Long.parseLong(tokens[2]);
+					if(HT.insert(key, val))
+						System.out.println("Key " + key + " inserted on list " + HT.hash(key, M));
+					else
+						System.out.println("Key " + key + " already exists");
 					break;
 				
 				case "D": // delete
-					
 					key = tokens[1];
-					
+					//if(HT.delete(key,val))
+						System.out.println("Key " + key + " deleted from list " + HT.hash(key, M));
+					//else
+						//System.out.println("Key " + key + " already exists");
 					break;
 				case "S": // search
 					key = tokens[1];
-				
+					if(HT.search(key) != -1)
+						System.out.println("Key " + key + " found on list " + HT.hash(key, M) + ", record = " + HT.search(key));
+					else
+						System.out.println("Key " + key + " doesn't exist");
 					break;
 				case "P": // print stats
-					//HT.printStats();
+					HT.printStats();
 					break;
 			} // end of switch
 		} // end of for
